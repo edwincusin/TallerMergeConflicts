074965782bf075427f69b2512b434ffef2a09bf4
michaelTipan <michaeltipan99@gmail.com>
2026-04-01 21:48:43 -0500

    Cambios del estudiante 1 en Product.js

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,11 +1,14 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
-export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+export const Product = ({ navigation }) => {
+    return <View style={styles.container}>
+        <Text style={styles.title}>Bienvenidos a mi tienda, soy Joanthan</Text>
+        <Text style={styles.product1}>Manzanas</Text>
+        <Text style={styles.product2}>Peras</Text>
+        <Text style={styles.product3}>Naranjas</Text>
         <Button
             title='HOME'
-            onPress={()=>{
+            onPress={() => {
                 navigation.navigate('HomeNav');
             }}
         />
@@ -15,9 +18,26 @@ export const Product = ({navigation}) =>{
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#e0ffff', // Color de fondo cambiado
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    product1: {
+        color: 'red',
+        fontSize: 16
+    },
+    product2: {
+        color: 'green',
+        fontSize: 16
+    },
+    product3: {
+        color: 'orange',
+        fontSize: 16
+    }
+});
