5962aa146a3c5a5422bb01a1f75ec3b7c95b14fc
michaelTipan <michaeltipan99@gmail.com>
2026-04-01 22:19:19 -0500

    Cambio en el App.js por CallNumber por Estudiante 1

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 
       <Stack.Navigator>
         <Stack.Screen name='WellcomeJonathan' component={Home} />
-        <Stack.Screen name='ContactsNav' component={Contacts} />
+        <Stack.Screen name='CallNumber' component={Contacts} />
         <Stack.Screen name='ProductNavigation' component={Product} />
 
 
